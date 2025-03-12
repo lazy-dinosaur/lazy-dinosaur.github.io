173905a18fb76fc60b8ef2a109aa4ce9264e8a55
lazydino <woohs0130@naver.com>
2025-03-12 15:35:55 +0900

    Style: 에니메이션 속도 더 빠르게 조정

--- app/(blog)/page.tsx
+++ app/(blog)/page.tsx
@@ -1,17 +1,18 @@
+"use client";
 import PostCard from "@/components/post-card";
-import { getPosts } from "@/lib/posts";
 import { HeaderSection, PostGrid, PostItem } from "@/components/home-animation";
+import { usePosts } from "@/contexts/posts-context";
+
 // Import global CSS classes for typography
 
-export default async function Home() {
-  const posts = await getPosts();
-
+export default function Home() {
+  const { posts } = usePosts();
   return (
     <div className="space-y-6 sm:space-y-8 2xl:space-y-10 h-full rounded-lg p-1 sm:p-7">
       {/* 헤더 섹션 */}
-      <HeaderSection 
-        title="Lazydino's DevLog" 
-        description="내가 한걸 티내기 위해 만든 블로그" 
+      <HeaderSection
+        title="Lazydino's DevLog"
+        description="내가 한걸 티내기 위해 만든 블로그"
       />
 
       {/* 포스트 그리드 */}
