6fcebbbabd433f7e70b32611498136f21500ea66
lazydino <woohs0130@naver.com>
2025-03-12 15:38:41 +0900

    Feat: 이미지 최적화

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 // import withExportImages from "next-export-optimize-images";
 
 const nextConfig: NextConfig = {
-  // output: "export",
+  output: "export",
   images: {
     domains: ["user-images.githubusercontent.com", "lazy-dinosaur.github.io"],
     remotePatterns: [
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
     ],
     // formats: ["image/webp"],
     // Remove unoptimized: true to enable image optimization
-    unoptimized: true,
+    // unoptimized: true,
   },
   trailingSlash: true, // ✅ 정적 서버 라우팅 호환성
   /* config options here */
