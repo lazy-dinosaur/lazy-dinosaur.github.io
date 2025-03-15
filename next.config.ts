19bd797d0361573870c07b2c99ff34615a482ef4
lazydino <woohs0130@naver.com>
2025-03-15 09:27:54 +0900

    update post

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  // output: "export",
   images: {
     loader: "default",
     domains: ["user-images.githubusercontent.com", "lazy-dinosaur.github.io"],
